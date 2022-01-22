3e8d703bab70f9ee90f37469741fa7956f024c85
faha1999 <kafahad1999@gmail.com>
2022-01-22 22:22:35 +0600

    complete nav without js function

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,20 @@
-// let menu = document.querySelector('#menu-btn');
-// let navbar = document.querySelector('.header .nav');
+let menu = document.querySelector('#customs');
+let navbar = document.querySelector('.header .navbar-nav');
 
-// menu.onclick = () => {
-//   menu.classList.toggle('fa-times');
-//   navbar.classList.toggle('active');
-// };
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  //menu.classList.add('fa-times');
+  //document.querySelector('.navbar-toggler').classList.add('fa-times');
+  navbar.classList.toggle('active');
+};
 
-// window.onscroll = () => {
-//   menu.classList.remove('fa-times');
-//   navbar.classList.remove('active');
+window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
 
-//   if (window.scrollY > 0) {
-//     document.querySelector('.header').classList.add('active');
-//   } else {
-//     document.querySelector('.header').classList.remove('active');
-//   }
-// };
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
