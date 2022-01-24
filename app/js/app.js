c483f21ede02860d2359ab5853545c7878e8d9e7
faha1999 <kafahad1999@gmail.com>
2022-01-25 00:03:23 +0600

    complete front-end

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,16 @@
 let menu = document.querySelector('#customs');
 let navbar = document.querySelector('.header .navbar-nav');
 
-menu.onclick = () => {
-  menu.classList.toggle('fa-times');
-  //menu.classList.add('fa-times'); fa-xmark
-  //document.querySelector('.navbar-toggler').classList.add('fa-times');
-  navbar.classList.toggle('active');
-};
+// menu.onclick = () => {
+//   menu.classList.toggle('fa-times');
+//   //menu.classList.add('fa-times'); fa-xmark
+//   //document.querySelector('.navbar-toggler').classList.add('fa-times');
+//   navbar.classList.toggle('active');
+// };
 
 window.onscroll = () => {
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
+  // menu.classList.remove('fa-times');
+  // navbar.classList.remove('active');
 
   if (window.scrollY > 0) {
     document.querySelector('.header').classList.add('active');
